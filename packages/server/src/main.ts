7cf2dea0aa250f8edd71db0e8c0ea78ef3d8fe19
Efim Pushkarev <pushkarevev@yandex.ru>
2025-05-19 16:48:53 +0300

    chore(server deps): Update dependencies versions
    - Updated NestJS from v10 to v11
    - Updated TypeORM from v0.3.17 to v0.3.24
    - Updated ESLint from v8 to v9
    - Other dependencies updates

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,17 +1,20 @@
 import 'reflect-metadata';
 
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from '~modules/App/app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
     app.setGlobalPrefix('api');
     app.enableCors({
         origin: true,
         allowedHeaders: ['GET', 'POST', 'DELETE', 'PATCH'],
         credentials: true,
     });
+
+    app.set('query parser', 'extended');
 
     await app.listen(4000);
 }
